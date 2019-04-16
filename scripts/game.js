9245b2b3eb203a442337f2d0b41712d1656e1540
louplemaire <44206840+louplemaire@users.noreply.github.com>
2019-04-16 15:44:39 +0200

    start JS of worm menu

--- scripts/game.js
+++ scripts/game.js
@@ -1,3 +1,5 @@
+//SubMenu setting
+
 const settingButton = document.querySelector('.settingButton')
 const subMenu = document.querySelector('.subMenuClose')
 
@@ -8,3 +10,16 @@ settingButton.addEventListener(
         subMenu.classList.toggle('subMenuClose')
     }
 )
+
+// //menuWorm
+
+// const yellowPlus = document.querySelector('.yellowPlus')
+// const /* nomDeLaDivQuiOuvreLeMenuWorm */ = document.querySelector(/* nomDeSaClass */)
+
+// yellowPlus.addEventListener(
+//     'click',
+//     function(){
+//         subMenu.classList.toggle(/* nom de la class au premier click pour ouvrir */)
+//         subMenu.classList.toggle(/* nom de la class au second click pour fermer */)
+//     }
+// )
