ac499900bf306446f6950d9003c1cde25b984997
louplemaire <44206840+louplemaire@users.noreply.github.com>
2019-04-18 19:15:47 +0200

    add function coin when i press $ key

--- scripts/game.js
+++ scripts/game.js
@@ -522,13 +522,21 @@ dynAdd.addEventListener(
 // >>>>>>> f4856b0f7439bca89d6fa861530f22e5edaa14e3
 
 
-// Activation de la dynamite
+// Dynamite activation
 const explosionUse = document.querySelector('.explosionSoundUse')
-window.addEventListener('keydown', function (event){
-    if(event.key == "d"){
-        // nom de la function de l'evenement causé par la dynamite
-        console.log('function actived') // voir si la function s'active à l'activation de la touche d
+window.addEventListener('keydown', function (eventExplosion){
+    if(eventExplosion.key == "d"){
+        // Name of the function dynamite
         explosionUse.play()
     }
 })
 
+const coinSound = document.querySelector('.coinSoundUse')
+const coinValue = 500
+window.addEventListener('keydown', function (eventCoin){
+    if(eventCoin.key == "$"){
+        TCoin += coinValue
+        totalCoin.innerHTML = TCoin
+        coinSound.play()
+    }
+})
